71976706f7c693945fd9143edcb362d611a164ce
Administrator <Administrator@WIN-51S15OO1E73>
2016-10-08 13:15:35 +0100

    Add RegEx Demo in Mod 3

---
+++ Module03aDemos/Scripts/regexSample.js
@@ -0,0 +1,16 @@
+﻿function TestDemo(re, teststring) {
+    // Test string for existence of regular expression.
+    var found = re.test(teststring)
+
+    // Format the output.
+    var s = "";
+    s += "'" + teststring + "'"
+
+    if (found)
+        s += " contains ";
+    else
+        s += " does not contain ";
+
+    s += "'" + re.source + "'"
+    return (s);
+}
