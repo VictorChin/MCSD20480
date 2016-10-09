90f8a17c6acd87629f3b06ad3502b6986d498358
Victor Chin <victor@objectmage.com>
2016-10-09 01:15:27 +0100

    Add Jquery Demos in 3B

---
+++ Module13Demos/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Module13Demos.Startup))]
+namespace Module13Demos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            Microsoft.AspNet.SignalR.StockTicker.Startup.ConfigureSignalR(app);
+
+        }
+    }
+}
